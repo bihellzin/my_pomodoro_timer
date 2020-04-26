3b969e25d4069203f494ed8e8db99e86a6b31bca
bihellzin <gof2@cin.ufpe.br>
2020-04-26 11:17:54 -0300

    added an example text file and a test .py file

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import time
+import optparse
+import sys
+
+
+def main():
+    '''
+    The main function of the app
+    '''
+    return
+
+
+def print_manual():
+    """
+    If the user puts an incorrect argument or if he calls the \"help\" command
+    this function will be called
+    """
+    print('The argument you just passed does not exist and blablabla')
+
+
+if __name__ == "__main__":
+    main()
+    print('rodou')
